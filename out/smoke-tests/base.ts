ba5fa6b0f6eba5a04d4c41b692b3ff12995d6c61
0xLadoo <sendev@rage.trade>
2025-07-01 20:42:03 +0530

    feat: Grouped orders impl

---
+++ out/smoke-tests/base.ts
@@ -0,0 +1,7 @@
+export const ACCOUNT_INDEX = 22853
+export const ETH_ADDRESS = '0xF858f15A36D4E526F331930069Cc19aabC013F3E'
+export const API_KEY_INDEX = 0
+export const SEED_PHRASE = 'my-secret-seed-phrase'
+export const SEED_PHRASE_2 = 'my-secret-seed-phrase-2'
+export const URL = 'https://mainnet.zklighter.elliot.ai/'
+export const CHAIN_ID = 304
